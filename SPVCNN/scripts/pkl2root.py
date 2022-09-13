6fe72e4b633e6d716bc0f4eef547b28a3eb7302c
hrzhao76 <hrzhao76@gmail.com>
2022-09-13 01:49:23 -0700

    write SPVCNN output branch

--- SPVCNN/scripts/pkl2root.py
+++ SPVCNN/scripts/pkl2root.py
@@ -12,6 +12,8 @@ import uproot
 import awkward as ak
 from pathlib import Path
 import os
+from tqdm import tqdm
+from array import array 
 
 # %%
 sample_folder_path = Path("Preds/dzp_pu10/preds/tbeta=0.75_td=0.4_pkls/")
@@ -22,18 +24,13 @@ file = files[0]
 data = pd.read_pickle(file)
 
 # %%
-m_eventID = int(file.stem[-5:])
+outputFile = ROOT.TFile("SPVCNN_outputs.root", "recreate")
+outputTree = ROOT.TTree("SPVCNN_Cluster", "SPVCNN_Cluster")
 
-# make pred id consecutive 
-new_label = np.zeros_like(data['pred_instance_labels'])
-for i, old_id in enumerate(np.unique(data['pred_instance_labels'])):
-    new_label[ data['pred_instance_labels'] == old_id] = i
-
-
-# %%
-reco_vtx_x0_seed = ROOT.std.vector[int]()
-reco_vtx_y0_seed = ROOT.std.vector[int]()
-reco_vtx_z0_seed = ROOT.std.vector[int]()
+m_eventID = array('i', [0])
+reco_vtx_x0_seed = ROOT.std.vector[float]()
+reco_vtx_y0_seed = ROOT.std.vector[float]()
+reco_vtx_z0_seed = ROOT.std.vector[float]()
 
 truth_vtx_fitted_trk_d0 = ROOT.std.vector[float]()
 truth_vtx_fitted_trk_z0 = ROOT.std.vector[float]()
@@ -63,17 +60,50 @@ fitted_trk_name_list = ['d0', 'z0', 'phi', 'theta', 'qp', 'time',
                         'err_d0', 'err_z0', 'err_phi', 'err_theta', 'err_qp', 'err_time', 
                         'reco_SPVCNN_vtxID']
 
+reco_vtx_list = [reco_vtx_x0_seed, reco_vtx_y0_seed, reco_vtx_z0_seed]
+reco_vtx_name_list = ['x0', 'y0', 'z0'] 
+
+outputTree.Branch("event_id", m_eventID, "eventID/I")
+
+for i, vtx_property in enumerate(reco_vtx_list):
+    outputTree.Branch(f"reco_vtx_{reco_vtx_name_list[i]}_seed", vtx_property)
+
+
+
+for j, trk_property in enumerate(fitted_trk_list[:-1]):
+    outputTree.Branch(f"truth_vtx_fitted_trk_{fitted_trk_name_list[j]}", trk_property)
+    
+outputTree.Branch("reco_SPVCNN_vtxID", truth_vtx_fitted_trk_SPVCNN_vtxID)
+
 
 # %%
-n_clusters = np.max(new_label) + 1 
-for i in range(n_clusters):
-    trk_idxs = np.where(new_label) == i
-    reco_vtx_x0_seed.push_back(0)
-    reco_vtx_x0_seed.push_back(0)
-    reco_vtx_z0_seed.push_back(np.mean(data.iloc[trk_idxs]['z0']))
+for file in tqdm(files):
+    data = pd.read_pickle(file)
+    m_eventID[0] = int(file.stem[-5:])
+    # make pred id consecutive 
+    new_label = np.zeros_like(data['pred_instance_labels'])
+    for i, old_id in enumerate(np.unique(data['pred_instance_labels'])):
+        new_label[ data['pred_instance_labels'] == old_id] = i
+
+    n_clusters = np.max(new_label) + 1 
     
-    for trk_idx in trk_idxs:
-        for j in range(len(fitted_trk_list)):
-            fitted_trk_list[j].push_back(data.iloc[trk_idx][fitted_trk_name_list[j]])
+    for i in range(n_clusters):
+        trk_idxs = np.where(new_label == i)
+        reco_vtx_x0_seed.push_back(0)
+        reco_vtx_y0_seed.push_back(0)
+        reco_vtx_z0_seed.push_back(np.mean(data.iloc[trk_idxs]['z0']))
+        
+        for trk_idx in trk_idxs[0]:
+            for j in range(len(fitted_trk_list)-1):
+                fitted_trk_list[j].push_back(data.iloc[trk_idx][fitted_trk_name_list[j]])
+            fitted_trk_list[12].push_back(i)
 
+        outputTree.Fill()
 
+        for j, vtx_property in enumerate(reco_vtx_list):
+            vtx_property.clear()
+        for j, trk_property in enumerate(fitted_trk_list):
+            trk_property.clear()
+
+outputTree.Write()
+outputFile.Close()
