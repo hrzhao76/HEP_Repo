57d7fefec53da63cefcfdb15f9dda5b0d481f2e4
hrzhao76 <hrzhao76@gmail.com>
2022-12-19 08:26:18 -0800

    example BDT training for EPEML

---
+++ QG_Calibration/BDT_EPEML/scripts/prepare_training.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+conda_activate_path=/global/cfs/cdirs/atlas/hrzhao/miniconda3/bin/activate
+source ${conda_activate_path} ml
+
+workdir=/global/cfs/projectdirs/atlas/hrzhao/qgcal/BDT_EPEML/scripts
+outputdir=/global/cfs/projectdirs/atlas/hrzhao/qgcal/BDT_EPEML/perpared_dijets
+sampledir=/global/cfs/projectdirs/atlas/hrzhao/qgcal/Samples_New
+
+period=${1}
+tree=nominal
+
+python -u ${workdir}/prepare_training.py --path ${sampledir}/pythia/pythia${period} --period ${period} --output-path ${outputdir}/Processed_Samples_Pythia_Nov8/period${period} --tree ${tree} | tee ${workdir}/log/log.perpare.Pythia.period${period}.txt
+
+# The following used to check if samples are complete. 
+# python -u ${workdir}/make_Histogram_Data_syst.py --path ${sampledir}/data/ --period ${period} --output-path ${outputdir}/Processed_Samples_Data_Oct18/ --reweight-file-path ${outputdir}/Processed_Samples_Pythia_Nov8/period${period} | tee ${workdir}/log/log.perpare.Data.period${period}.txt
